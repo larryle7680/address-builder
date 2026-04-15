027a05d53f0200939aa68f0afab76f81d6a43463
Larry <larryle7680@gmail.com>
2026-04-15 15:52:38 -0500

    Finish the string builder project

--- src/main/java/com/pluralsight/AddressBuilderApp.java
+++ src/main/java/com/pluralsight/AddressBuilderApp.java
@@ -1,4 +1,42 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class AddressBuilderApp {
+    //importing Scanner/String Builder in class
+    static Scanner theScanner = new Scanner(System.in);
+    static StringBuilder stringGlue = new StringBuilder();
+
+    //Main
+    public static void main(String[] args) {
+        String name = askQuestion("What is your name?");
+        String billingStreet = askQuestion("What is your street?");
+        String city = askQuestion("What is your city?");
+        String state = askQuestion("What is your state?");
+        String zip = askQuestion("What is your zip code?");
+
+        createAddress(name,billingStreet,city,state
+        ,zip);
+
+        System.out.println(stringGlue);
+
+
+
+
+
+    }
+    //Question Method
+    public static String askQuestion(String question) {
+        System.out.println(question);
+        return theScanner.nextLine();
+    }
+
+    public static void createAddress(String name, String billingStreet, String city,String state,String zip){
+        stringGlue.append("---Your Billing Address---\n");
+        stringGlue.append("Name:" + name + "\n");
+        stringGlue.append("Billing Street:" + billingStreet + "\n");
+        stringGlue.append("City:" + city + "\n");
+        stringGlue.append("State:" + state + "\n");
+        stringGlue.append("Zip:" + zip + "\n");
+
+    }
 }
